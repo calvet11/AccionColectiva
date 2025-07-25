a3aec257a4e1a5d042e74cd99a548ddebac22d58
gac797 <gabamiel32gmail.com>
2025-07-25 18:37:22 -0300

    configuracion de firebase con proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import '/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // importá este archivo generado
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // <- aquí usás las opciones
+  );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
